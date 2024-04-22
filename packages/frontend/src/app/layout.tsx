2741e2516a267ac60d0e2d0e06f9144f96615671
ddfilipov <denisfilipov12+3@gmail.com>
2024-04-22 18:49:01 +0200

    common layout added

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -1,10 +1,13 @@
+"use client";
+
 import type { Metadata } from "next";
 import "./globals.css";
 import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 import GeneralError from "./error";
-import StyledComponentsRegistry from "src/lib/StyledJsxRegistry";
+import StyledComponentsRegistry from "./registry";
+import styled from "styled-components";
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
     title: "Home",
     description: "Homepage",
 };
@@ -17,8 +20,26 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
-                <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+                <MainContainer>
+                    <ChildrenContainer>
+                        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+                    </ChildrenContainer>
+                </MainContainer>
             </body>
         </html>
     );
 }
+export const MainContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    height: 100vh;
+    padding: 20px;
+`;
+
+export const ChildrenContainer = styled.div`
+    border: 1px solid #400;
+    display: flex;
+    justify-content: center;
+    width: 1200px;
+    padding: 20px;
+`;
--- packages/frontend/src/components/MainArea.tsx
+++ packages/frontend/src/components/MainArea.tsx
@@ -3,7 +3,6 @@ import { FC, useState } from "react";
 
 import UncontrolledClientError from "./UncontrolledClientError";
 
-import * as Styled from "./styled";
 import { EmployeeData } from "@common/types/index";
 
 interface MainAreaProps {
@@ -20,23 +19,7 @@ const MainArea: FC<MainAreaProps> = ({ companyData }) => {
     if (errores) {
         throw new Error("algo ha pasado...");
     }
-    return (
-        <Styled.MainContainer>
-            <Styled.ChildrenContainer>
-                <UncontrolledClientError employeeData={companyData.employees} address={companyData.address} />
-            </Styled.ChildrenContainer>
-        </Styled.MainContainer>
-    );
+    return <UncontrolledClientError employeeData={companyData.employees} address={companyData.address} />;
 };
-
-function printLine(): boolean {
-    return true;
-    try {
-        throw new Error("Client error!");
-    } catch {
-        console.error("error while clicking button");
-    }
-    console.log("I'm clicking the button!");
-}
 
 export default MainArea;
