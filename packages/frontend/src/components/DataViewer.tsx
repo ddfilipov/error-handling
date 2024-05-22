d8f93240de2e18d0543abd2cfee89195ca122f8f
ddfilipov <denisfilipov12+3@gmail.com>
2024-05-22 19:38:30 +0200

    adding padding to pre

--- packages/frontend/src/components/DataViewer.tsx
+++ packages/frontend/src/components/DataViewer.tsx
@@ -1,10 +1,16 @@
+"use client";
 import { Company } from "@common/types";
 import { FC } from "react";
+import styled from "styled-components";
 
 interface DataViewerProps {
     data: Company;
 }
 
 export const DataViewer: FC<DataViewerProps> = ({ data }) => {
-    return <pre>{JSON.stringify(data, null, 2)}</pre>;
+    return <Container>{JSON.stringify(data, null, 2)}</Container>;
 };
+
+const Container = styled.pre`
+    padding: 16px;
+`;
