3882edffae7217af5532b5e7ce228de1ee664b8d
ddfilipov <denisfilipov12+3@gmail.com>
2024-05-22 18:42:36 +0200

    clarifying what child component does

--- packages/frontend/src/components/client-errors/ErrorBoundaryComponent.tsx
+++ packages/frontend/src/components/client-errors/ErrorBoundaryComponent.tsx
@@ -3,7 +3,7 @@ import { FC, useEffect } from "react";
 import ErrorBoundary from "../ErrorBoundary";
 import ErrorBoundaryLayout from "../layout/ErrorBoundaryLayout";
 
-const ComponentThatThrowsError = () => {
+const ChildComponentThatThrowsError = () => {
     useEffect(() => {
         throw new Error("This is an error");
     }, []);
@@ -11,9 +11,10 @@ const ComponentThatThrowsError = () => {
     return <div>{`Will never see this div cuz there's an error before`}</div>;
 };
 export const ErrorBoundaryComponent: FC = ({}) => {
+    
     return (
         <ErrorBoundary fallbackComponent={<ErrorBoundaryLayout />}>
-            <ComponentThatThrowsError />
+            <ChildComponentThatThrowsError />
         </ErrorBoundary>
     );
 };
